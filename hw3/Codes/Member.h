e328f188e2a9b641ccdb61022a1aba28ba068614
hanslelee <hssarah13@gmail.com>
2022-05-29 20:25:00 +0900

    커뮤니케이션 다이어그램, 디자인클래스다이어그램 피드백 받은걸로 수정

--- hw3/Codes/Member.h
+++ hw3/Codes/Member.h
@@ -2,17 +2,18 @@
 
 using namespace std;
 
+// 멤
 class Member
 {
 private:
-    String memberId;
-    String memberPw;
+    String memberId;	// 멤버 이름
+    String memberPw;	// 멤버 비밀번호
 	String memberName;
     String residentRegistrationNum;
 
 public:
-	void listProductsOnSale(); 
-	void listSoldProducts(); 
+	void listProductsOnSale(); 	// 판매중인 상품 리스트 가져오기
+	void listSoldProducts(); 	// 판매한 상품들 리스트 가져오기
 	void listOrders(); 
 	void deleteMember(); 
 	void checkidpw();
